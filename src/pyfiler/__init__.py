3b784e8c5496201551795d9c05df369b23b1848a
Leila150 <esguerradominguita@gmail.com>
2026-08-24 22:42:04 +0800

    Fix public compatibility aliases

--- src/pyfiler/__init__.py
+++ src/pyfiler/__init__.py
@@ -1,11 +1,15 @@
 """Public API for pyfiler.
 
-Canonical APIs are explicitly exported here; compatibility aliases remain
-available from their implementation modules and ``pyfiler.operations``.
+Canonical APIs and supported compatibility aliases are exported here.
 """
 from .explorer import Explorer
 from .storage import StorageInfo, setup_storage
-from .files import read_contents, append_contents, remove_contents, replace_contents, create_file, delete_file, copy_file, move_file, rename_file, touch_file, file_exists, clear_file
+from .files import (
+    read_contents, append_contents, remove_contents, replace_contents,
+    create_file, delete_file, copy_file, move_file, rename_file, touch_file,
+    file_exists, clear_file, get_contents, add_contents, edit_contents,
+    read_file, write_file, append_file,
+)
 from .folders import create_folder, move_into, delete_folder, list_folder, remove_from_folder, list_files, list_folders, clear_folder, copy_folder, move_folder, rename_folder
 from .search import find_paths, find_files, find_folders, find_text, find_pattern, find_by_extension, find_by_size
 from .paths import absolute_path, relative_path, parent_path, file_name, file_extension, file_stem, join_paths, normalize_path, is_absolute, is_relative, has_parent, path_parts, with_name, with_extension, is_root, common_path, relative_to, add_suffix
@@ -19,6 +23,7 @@ from .exceptions import *
 __all__ = [
     "Explorer", "StorageInfo", "setup_storage",
     "read_contents", "append_contents", "remove_contents", "replace_contents", "create_file", "delete_file", "copy_file", "move_file", "rename_file", "touch_file", "file_exists", "clear_file",
+    "get_contents", "add_contents", "edit_contents", "read_file", "write_file", "append_file",
     "create_folder", "move_into", "delete_folder", "list_folder", "remove_from_folder", "list_files", "list_folders", "clear_folder", "copy_folder", "move_folder", "rename_folder",
     "find_paths", "find_files", "find_folders", "find_text", "find_pattern", "find_by_extension", "find_by_size",
     "absolute_path", "relative_path", "parent_path", "file_name", "file_extension", "file_stem", "join_paths", "normalize_path", "is_absolute", "is_relative", "has_parent", "path_parts", "with_name", "with_extension", "is_root", "common_path", "relative_to", "add_suffix",
@@ -29,4 +34,4 @@ for _name, _value in vars(_exceptions).items():
     if not _name.startswith("_") and isinstance(_value, type) and _value.__module__ == _exceptions.__name__:
         __all__.append(_name)
 __all__ = list(dict.fromkeys(__all__))
-__version__ = "0.2.3"
+__version__ = "0.2.4"
