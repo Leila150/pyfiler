524a1a95e199efccbc3cfadd4dcc2e852e3fbbec
Leila150 <esguerradominguita@gmail.com>
2026-08-25 16:45:12 +0800

    Replace setup_storage with storage_info API

--- src/pyfiler/__init__.py
+++ src/pyfiler/__init__.py
@@ -3,7 +3,8 @@
 Canonical APIs and supported compatibility aliases are exported here.
 """
 from .explorer import Explorer
-from .storage import StorageInfo, setup_storage
+from .storage_info import StorageStatus
+from . import storage_info
 from .files import (
     read_contents, append_contents, remove_contents, replace_contents,
     create_file, delete_file, copy_file, move_file, rename_file, touch_file,
@@ -41,40 +42,30 @@ from . import exceptions as _exceptions
 from .exceptions import *
 
 __all__ = [
-    "Explorer", "StorageInfo", "setup_storage",
+    "Explorer", "StorageStatus", "storage_info",
     "read_contents", "append_contents", "remove_contents", "replace_contents",
     "create_file", "delete_file", "copy_file", "move_file", "rename_file",
-    "touch_file", "file_exists", "clear_file",
-    "get_contents", "add_contents", "edit_contents", "read_file", "write_file",
-    "append_file",
+    "touch_file", "file_exists", "clear_file", "get_contents", "add_contents",
+    "edit_contents", "read_file", "write_file", "append_file",
     "create_folder", "move_into", "delete_folder", "list_folder",
     "remove_from_folder", "list_files", "list_folders", "clear_folder",
     "copy_folder", "move_folder", "rename_folder", "add_parent",
     "folder_contents", "folder_remove_contents", "list_dir",
     "find_paths", "find_files", "find_folders", "find_text", "find_pattern",
-    "find_by_extension", "find_by_size", "find", "search_contents",
-    "search_regex", "find_extension",
-    "absolute_path", "relative_path", "parent_path", "file_name",
-    "file_extension", "file_stem", "join_paths", "normalize_path",
-    "is_absolute", "is_relative", "has_parent", "path_parts", "with_name",
-    "with_extension", "is_root", "common_path", "relative_to", "add_suffix",
-    "absolute", "relative", "parent", "filename", "extension", "stem",
-    "join", "normalize", "parts",
-    "exists", "is_file", "is_folder", "is_empty", "size_of", "extension_of",
-    "created_at", "modified_at", "accessed_at", "metadata", "permissions",
-    "name_of", "stem_of", "path_kind", "same_type", "is_symlink", "device_id",
-    "inode", "mode", "nlink", "same_filesystem", "same_object",
-    "file_hash", "hash_algorithms", "files_equal", "directory_tree",
-    "directory_size", "count_files", "count_folders", "extension_counts",
+    "find_by_extension", "find_by_size", "find", "search_contents", "search_regex", "find_extension",
+    "absolute_path", "relative_path", "parent_path", "file_name", "file_extension", "file_stem",
+    "join_paths", "normalize_path", "is_absolute", "is_relative", "has_parent", "path_parts",
+    "with_name", "with_extension", "is_root", "common_path", "relative_to", "add_suffix",
+    "absolute", "relative", "parent", "filename", "extension", "stem", "join", "normalize", "parts",
+    "exists", "is_file", "is_folder", "is_empty", "size_of", "extension_of", "created_at", "modified_at",
+    "accessed_at", "metadata", "permissions", "name_of", "stem_of", "path_kind", "same_type",
+    "is_symlink", "device_id", "inode", "mode", "nlink", "same_filesystem", "same_object",
+    "file_hash", "hash_algorithms", "files_equal", "directory_tree", "directory_size", "count_files",
+    "count_folders", "extension_counts",
 ]
 
 for _name, _value in vars(_exceptions).items():
-    if (
-        not _name.startswith("_")
-        and isinstance(_value, type)
-        and _value.__module__ == _exceptions.__name__
-    ):
+    if not _name.startswith("_") and isinstance(_value, type) and _value.__module__ == _exceptions.__name__:
         __all__.append(_name)
-
 __all__ = list(dict.fromkeys(__all__))
 __version__ = "0.2.5"
